40b5d8989611f56804510d7fb033e1cdc8d93a38
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-10-30 18:48:49 +0530

    "ads2_m2_a1_1: 6/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 2/2.1 CC_Percolation/Solution.java
+++ Assignments/Module 2/2.1 CC_Percolation/Solution.java
@@ -1,17 +1,29 @@
-import java.util.Scanner; 
-class Solution {
- 	Solution() {
-
- 	}
- 	public static void main(String[] args) {
- 		Scanner sc = new Scanner(System.in);
- 		int sizeofgrid = Integer.parseInt(sc.nextLine());
- 		Percolation perco = new Percolation(sizeofgrid);
- 		while(sc.hasNext()) {
- 			String[] tokens = sc.nextLine().split(" ");
- 			perco.open(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
- 		}
- 		System.out.println(perco.percolates());
-
- 	}
- }
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    protected Solution() {
+        //unused constructor.
+    }
+    /**
+     * Client program.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        Percolation pobj = new Percolation(n);
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
+            pobj.open(Integer.parseInt(tokens[0]),
+                      Integer.parseInt(tokens[1]));
+        }
+        System.out.println(pobj.percolates()
+                           && pobj.numberOfOpenSites() != 0);
+    }
+}
