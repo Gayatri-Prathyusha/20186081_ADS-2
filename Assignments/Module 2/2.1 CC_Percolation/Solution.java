2c8716f751adaf925aae0172aec8cf366e4c5b1e
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-10-30 18:44:15 +0530

    "ads2_m2_a1_1: 6/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 2/2.1 CC_Percolation/Solution.java
+++ Assignments/Module 2/2.1 CC_Percolation/Solution.java
@@ -5,15 +5,12 @@ class Solution {
  	}
  	public static void main(String[] args) {
  		Scanner sc = new Scanner(System.in);
-
  		int sizeofgrid = Integer.parseInt(sc.nextLine());
- 		Graph graph = new Graph(sizeofgrid * sizeofgrid); 		
+ 		Percolation perco = new Percolation(sizeofgrid);
  		while(sc.hasNext()) {
  			String[] tokens = sc.nextLine().split(" ");
- 			graph.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
-            graph.hasEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+ 			perco.open(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
  		}
- 		Percolation perco = new Percolation(sizeofgrid);
  		System.out.println(perco.percolates());
 
  	}
