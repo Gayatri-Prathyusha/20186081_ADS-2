0e79a0123f34708caa114374b7ee42e1eccc6f84
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-11-01 00:40:39 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/Module 3/3.2 Bipartite/Queue.java
+++ Assignments/Module 3/3.2 Bipartite/Queue.java
@@ -23,105 +23,163 @@ import java.util.NoSuchElementException;
  *  <p>
  *  All queue operations except iteration are constant time.
  *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ *  For additional documentation,
+ *  see <a href="http://algs4.cs.princeton.edu/13stacks">
+ *  Section 1.3</a> of
  *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  */
+/**
+ * List of .
+ *
+ * @param      <Item>  The item
+ */
 public class Queue<Item> implements Iterable<Item> {
-    private int N;         // number of elements on queue
-    private Node first;    // beginning of queue
-    private Node last;     // end of queue
+    /**
+     * number of elements on queue
+     */
+    private int no;
+    /**
+     * beginning of queue
+     */
+    private Node first;
+    /**
+     * end of queue
+     */
+    private Node last;
 
-    // helper linked list class
+    /**
+     * Class for node.
+     * helper linked list class
+     */
     private class Node {
+        /**
+         * item.
+         */
         private Item item;
+        /**
+         * next.
+         */
         private Node next;
     }
-
-   /**
+    /**
+     * Constructs the object.
      * Create an empty queue.
      */
     public Queue() {
         first = null;
         last  = null;
     }
-
-   /**
+    /**
+     * Determines if empty.
      * Is the queue empty?
+     *
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return first == null;
     }
-
-   /**
+    /**
      * Return the number of items in the queue.
+     *
+     * @return     int.
      */
     public int size() {
-        return N;     
+        return no;     
     }
-
-   /**
+    /**
      * Return the item least recently added to the queue.
      * Throw an exception if the queue is empty.
+     *
+     * @return     Item.
      */
     public Item peek() {
-        if (isEmpty()) 
+        if (isEmpty()) {
             throw new RuntimeException("Queue underflow");
+        }
         return first.item;
     }
-
-   /**
+    /**
      * Add the item to the queue.
+     *
+     * @param      item  The item
      */
-    public void enqueue(Item item) {
+    public void enqueue(final Item item) {
         Node x = new Node();
         x.item = item;
         if (isEmpty()) { first = x;     last = x; }
         else           { last.next = x; last = x; }
-        N++;
+        no++;
     }
-
-   /**
+    /**
      * Remove and return the item on the queue least recently added.
      * Throw an exception if the queue is empty.
+     *
+     * @return     Item.
      */
     public Item dequeue() {
         if (isEmpty()) throw new RuntimeException("Queue underflow");
         Item item = first.item;
         first = first.next;
-        N--;
+        no--;
         if (isEmpty()) last = null;   // to avoid loitering
         return item;
     }
-
-   /**
-     * Return string representation.
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
      */
     public String toString() {
         StringBuilder s = new StringBuilder();
-        for (Item item : this)
+        for (Item item : this) {
             s.append(item + " ");
+        }
         return s.toString();
-    } 
- 
-
-   /**
+    }
+    /**
      * Return an iterator that iterates over the items on the queue in FIFO order.
+     *
+     * @return     List Iterable.
      */
     public Iterator<Item> iterator()  {
         return new ListIterator();  
     }
-
-    // an iterator, doesn't implement remove() since it's optional
+    /**
+     * an iterator, doesn't implement remove() since it's optional.
+     * Class for list iterator.
+     */
     private class ListIterator implements Iterator<Item> {
+        /**
+         * current node.
+         */
         private Node current = first;
 
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            return current != null;
+        }
+        /**
+         * remove method.
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
 
+        /**
+         * next method.
+         *
+         * @return     { description_of_the_return_value }
+         */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
