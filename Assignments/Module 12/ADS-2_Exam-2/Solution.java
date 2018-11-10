487c2d33f2f8c18ba789502f45a99a25acf7b62f
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-11-10 10:46:01 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ Assignments/Module 12/ADS-2_Exam-2/Solution.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+/**
+ * Class for solution.
+ */
+public class Solution {
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
+    // unused constructor
+  }
+  /**
+   * main method.
+   *
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    // Self loops are not allowed...
+    // Parallel Edges are allowed...
+    // Take the Graph input here...
+    Scanner scan = new Scanner(System.in);
+    int n = Integer.parseInt(scan.nextLine());
+    int k = Integer.parseInt(scan.nextLine());
+    EdgeWeightedGraph edgeweightgraph = new EdgeWeightedGraph(n);
+
+    for (int i = 0; i < k; i++) {
+      String[] inputs = scan.nextLine().split(" ");
+      int v = Integer.parseInt(inputs[0]);
+      int w = Integer.parseInt(inputs[1]);
+      double weight = Double.parseDouble(inputs[2]);
+      Edge e = new Edge(v, w, weight);
+      edgeweightgraph.addEdge(e);
+    }
+
+    String input = scan.nextLine();
+    switch (input) {
+    case "Graph":
+      System.out.println(edgeweightgraph);
+      break;
+
+    case "DirectedPaths":
+      // Handle the case of DirectedPaths, where two integers are given.
+      // First is the source and second is the destination.
+      // If the path exists print the distance between them.
+      // Other wise print "No Path Found."
+      break;
+
+    case "ViaPaths":
+      // Handle the case of ViaPaths, where three integers are given.
+      // First is the source and second is the via is the one where path should pass throuh.
+      // third is the destination.
+      // If the path exists print the distance between them.
+      // Other wise print "No Path Found."
+      break;
+
+    default:
+      break;
+    }
+
+  }
+}
